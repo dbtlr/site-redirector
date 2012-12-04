9a8aa253860e4823f64e48bae010294c36d5d740
Drew Butler <drew@nodrew.com>
2012-12-04 00:32:55 -0500

    remove test code, make simple redirect.

--- web.rb
+++ web.rb
@@ -4,13 +4,12 @@ require 'socket'
 
 get '*' do
   host     = Socket.gethostname
-  new_host = "nodrew.com"
+  new_host = "blog.nodrew.com"
   host_map = {'Drews-MacBook-Pro.local' => 'nodrew.com', 'nodrew.com' => 'blog.nodrew.com', 'www.nodrew.com' => 'blog.nodrew.com'}
 
   if host_map[host] 
     new_host = host_map[host]
   end
 
-  p HOSTNAME
-  p "http://"+new_host+params['splat'][0]+host
+  redirect "http://"+new_host+params['splat'][0]+host
 end
