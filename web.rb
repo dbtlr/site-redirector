eec6bc3e279af8da1bb900c3dd2957b2d8d9d5d7
Drew Butler <drew@nodrew.com>
2012-12-04 00:21:03 -0500

    pushing test code.

--- web.rb
+++ web.rb
@@ -6,10 +6,10 @@ get '*' do
   host     = Socket.gethostname
   new_host = "nodrew.com"
   host_map = {'Drews-MacBook-Pro.local' => 'nodrew.com', 'nodrew.com' => 'blog.nodrew.com', 'www.nodrew.com' => 'blog.nodrew.com'}
-  p host
+
   if host_map[host] 
     new_host = host_map[host]
   end
 
-  p "http://"+new_host+params['splat'][0]
+  p "http://"+new_host+params['splat'][0]+host
 end
