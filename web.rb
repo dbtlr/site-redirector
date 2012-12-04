4e146ed36844ee4f012c41dfcfd20b7e09c7a172
Drew Butler <drew@nodrew.com>
2012-12-04 00:14:18 -0500

    Made the app more generic.

--- web.rb
+++ web.rb
@@ -1,6 +1,17 @@
 require 'rubygems'
 require 'sinatra'
+require 'socket'
 
-get '/' do
-  redirect "http://blog.nodrew.com"
+get '*' do
+  host     = Socket.gethostname
+  new_host = "nodrew.com"
+  
+  host_map = {'nodrew.com' => 'blog.nodrew.com', 'www.nodrew.com' => 'blog.nodrew.com'}
+  
+  if host_map[host] 
+    new_host = host_map[host]
+  end
+
+  p "http://"+new_host+"/"+params['splat'][0]
+  redirect "http://"+new_host+"/"+params['splat'][0]
 end
