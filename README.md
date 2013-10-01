5b2b742ddbdcb8aa3bce9384cb4a63198f6605af
Drew Butler <hi@dbtlr.com>
2013-10-01 17:50:48 -0400

    Added license, readme and gitignore.

---
+++ README.md
@@ -0,0 +1,4 @@
+Simple Site Redirector
+----------------------
+
+This is a small app I wrote and host with a free Heroku instance, that allows for me to easily redirect domains and subdomains to other places. Overall this app doesn't do a whole lot, it is mostly here for storage and in case it helps someone else.
--- web.rb
+++ web.rb
@@ -4,12 +4,12 @@ require 'socket'
 
 get '*' do
   host     = Socket.gethostname
-  new_host = "dbtlr.com"
+  new_host = 'dbtlr.com'
   host_map = {'nodrew.com' => 'dbtlr.com', 'www.nodrew.com' => 'dbtlr.com'}
 
   if host_map[host] 
     new_host = host_map[host]
   end
 
-  redirect "http://"+new_host+params['splat'][0]
+  redirect 'http://' + new_host+params['splat'][0]
 end
